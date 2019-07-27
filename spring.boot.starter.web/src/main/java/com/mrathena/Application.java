0aef74389eba74e04d88247ef63c619c8f5d52f8
hushirui <hushirui-hb@bestpay.com.cn>
2019-07-27 16:48:59 +0800

    .

--- spring.boot.starter.web/src/main/java/com/mrathena/Application.java
+++ spring.boot.starter.web/src/main/java/com/mrathena/Application.java
@@ -3,6 +3,9 @@ package com.mrathena;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author mrathena on 2019/7/27 16:00
+ */
 @SpringBootApplication
 public class Application {
 
--- spring.boot.starter.web/src/main/java/com/mrathena/controller/TestController.java
+++ spring.boot.starter.web/src/main/java/com/mrathena/controller/TestController.java
@@ -3,6 +3,7 @@ package com.mrathena.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 /**
- * @author mrathena on 2019/7/24 10:05
+ * @author mrathena on 2019/7/27 16:01
  */
 @Slf4j
 @RestController
-@RequestMapping("index")
-public class IndexController {
+@RequestMapping("test")
+public class TestController {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+
+	@GetMapping("index")
+	public String index() {
+		return "Hello World";
+	}
 
 	@RequestMapping("customer/{id}")
 	public Object user(@PathVariable Long id) {
