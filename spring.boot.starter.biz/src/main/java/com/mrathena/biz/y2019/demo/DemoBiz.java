3464fd5e380b2f5c9de2a58b98f7eb723f4d6fed
hushirui <hushirui-hb@bestpay.com.cn>
2019-11-15 08:53:24 +0800

    .

--- spring.boot.starter.biz/src/main/java/com/mrathena/biz/y2019/demo/DemoBiz.java
+++ spring.boot.starter.biz/src/main/java/com/mrathena/biz/y2019/demo/DemoBiz.java
@@ -5,11 +5,11 @@ import com.mrathena.common.exception.ServiceException;
 import com.mrathena.common.toolkit.ParameterKit;
 import com.mrathena.dao.entity.y2019.demo.DemoDO;
 import com.mrathena.dao.manager.y2019.demo.DemoManager;
-import com.mrathena.spring.boot.starter.api.BasePageResDto;
-import com.mrathena.spring.boot.starter.api.y2019.demo.CreateDemoReqDto;
-import com.mrathena.spring.boot.starter.api.y2019.demo.DemoDto;
-import com.mrathena.spring.boot.starter.api.y2019.demo.QueryDemoReqDto;
-import com.mrathena.spring.boot.starter.api.y2019.demo.QueryDemoResDto;
+import com.mrathena.spring.boot.starter.api.BasePageResDTO;
+import com.mrathena.spring.boot.starter.api.y2019.demo.CreateDemoReqDTO;
+import com.mrathena.spring.boot.starter.api.y2019.demo.DemoDTO;
+import com.mrathena.spring.boot.starter.api.y2019.demo.QueryDemoReqDTO;
+import com.mrathena.spring.boot.starter.api.y2019.demo.QueryDemoResDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Component;
@@ -28,25 +28,25 @@ public class DemoBiz {
 	@Resource
 	private DemoConverter demoConverter;
 
-	public boolean createDemo(CreateDemoReqDto request) {
+	public boolean createDemo(CreateDemoReqDTO request) {
 		DemoDO demoDO = new DemoDO();
 		demoDO.setDemo(request.getDemo());
 		return demoManager.create(demoDO);
 	}
 
-	public QueryDemoResDto queryDemo(QueryDemoReqDto request) {
+	public QueryDemoResDTO queryDemo(QueryDemoReqDTO request) {
 		DemoDO demoDO = demoManager.queryDemoById(request.getId());
 		if (ObjectUtils.isEmpty(demoDO)) {
 			throw new ServiceException("Demo不存在", "没有查到数据");
 		}
-		QueryDemoResDto result = new QueryDemoResDto();
+		QueryDemoResDTO result = new QueryDemoResDTO();
 		result.setDemo(demoDO.getDemo());
 		return result;
 	}
 
-	public BasePageResDto<DemoDto> queryDemoWithPage(QueryDemoReqDto request) throws Exception {
+	public BasePageResDTO<DemoDTO> queryDemoWithPage(QueryDemoReqDTO request) throws Exception {
 		PageInfo<DemoDO> page = demoManager.queryDemoListByMapWithPage(ParameterKit.beanToMap(request), request.getPageSize(), request.getPageNo());
-		return new BasePageResDto<>(page.getTotal(), demoConverter.to(page.getList()));
+		return new BasePageResDTO<>(page.getTotal(), demoConverter.to(page.getList()));
 	}
 
 }
--- spring.boot.starter.biz/src/main/java/com/mrathena/biz/y2019/demo/DemoConverter.java
+++ spring.boot.starter.biz/src/main/java/com/mrathena/biz/y2019/demo/DemoConverter.java
@@ -2,7 +2,7 @@ package com.mrathena.biz.y2019.demo;
 
 import com.mrathena.biz.toolkit.BaseConverter;
 import com.mrathena.dao.entity.y2019.demo.DemoDO;
-import com.mrathena.spring.boot.starter.api.y2019.demo.DemoDto;
+import com.mrathena.spring.boot.starter.api.y2019.demo.DemoDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,7 +10,7 @@ import org.mapstruct.Mapping;
  * @author mrathena on 2019/11/12 18:22
  */
 @Mapper(componentModel = "spring")
-public interface DemoConverter extends BaseConverter<DemoDO, DemoDto> {
+public interface DemoConverter extends BaseConverter<DemoDO, DemoDTO> {
 
 	/**
 	 * to
@@ -20,7 +20,7 @@ public interface DemoConverter extends BaseConverter<DemoDO, DemoDto> {
 	 */
 	@Mapping(source = "demo", target = "anotherDemo")
 	@Override
-	DemoDto to(DemoDO source);
+	DemoDTO to(DemoDO source);
 
 	/**
 	 * from
@@ -30,6 +30,6 @@ public interface DemoConverter extends BaseConverter<DemoDO, DemoDto> {
 	 */
 	@Mapping(source = "anotherDemo", target = "demo")
 	@Override
-	DemoDO from(DemoDto source);
+	DemoDO from(DemoDTO source);
 
 }
