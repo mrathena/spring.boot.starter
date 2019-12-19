716edbb935044ee308c1c25230659c6e2fab88b6
hushirui <hushirui-hb@bestpay.com.cn>
2019-12-19 16:56:27 +0800

    .

--- spring.boot.starter.web/src/main/java/com/mrathena/web/configuration/CacheConfig.java
+++ spring.boot.starter.web/src/main/java/com/mrathena/web/configuration/CacheConfig.java
@@ -36,43 +36,25 @@ public class CacheConfig {
 				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer))
 				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer))
 				.disableCachingNullValues();
-		// RedisCacheConfiguration daily weekly monthly yearly eternally
-		RedisCacheConfiguration oneMinuteRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(1));
-		RedisCacheConfiguration fiveMinuteRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(5));
-		RedisCacheConfiguration tenMinuteRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(10));
-		RedisCacheConfiguration thirtyMinuteRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(30));
-		RedisCacheConfiguration oneHourRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofHours(1));
-		RedisCacheConfiguration sixHourRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofHours(6));
-		RedisCacheConfiguration twelveHourRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofHours(12));
-		RedisCacheConfiguration oneDayRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(1));
-		RedisCacheConfiguration sevenDayRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(7));
-		RedisCacheConfiguration fourteenDayRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(7));
-		RedisCacheConfiguration oneMonthRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(31));
-		RedisCacheConfiguration threeMonthRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(93));
-		RedisCacheConfiguration sixMonthRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(182));
-		RedisCacheConfiguration oneYearRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ofDays(366));
-		// Duration.ZERO 存入redis的时候是永久
-		RedisCacheConfiguration foreverRedisCacheConfiguration = commonRedisCacheConfiguration.entryTtl(Duration.ZERO);
 		// CacheConfigurationsMap
 		Map<String, RedisCacheConfiguration> cacheConfigurationMap = new HashMap<>(8);
-		cacheConfigurationMap.put(CacheNameEnum.ONE_MINUTE.name(), oneMinuteRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.FIVE_MINUTE.name(), fiveMinuteRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.TEN_MINUTE.name(), tenMinuteRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.THIRTY_MINUTE.name(), thirtyMinuteRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.ONE_HOUR.name(), oneHourRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.SIX_HOUR.name(), sixHourRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.TWELVE_HOUR.name(), twelveHourRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.ONE_DAY.name(), oneDayRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.SEVEN_DAY.name(), sevenDayRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.FOURTEEN_DAY.name(), fourteenDayRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.ONE_MONTH.name(), oneMonthRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.THREE_MONTH.name(), threeMonthRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.SIX_MONTH.name(), sixMonthRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.ONE_YEAR.name(), oneYearRedisCacheConfiguration);
-		cacheConfigurationMap.put(CacheNameEnum.FOREVER.name(), foreverRedisCacheConfiguration);
+		cacheConfigurationMap.put(CacheNameEnum.ONE_MINUTE.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(1)));
+		cacheConfigurationMap.put(CacheNameEnum.FIVE_MINUTE.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(5)));
+		cacheConfigurationMap.put(CacheNameEnum.TEN_MINUTE.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(10)));
+		cacheConfigurationMap.put(CacheNameEnum.THIRTY_MINUTE.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofMinutes(30)));
+		cacheConfigurationMap.put(CacheNameEnum.ONE_HOUR.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofHours(1)));
+		cacheConfigurationMap.put(CacheNameEnum.SIX_HOUR.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofHours(6)));
+		cacheConfigurationMap.put(CacheNameEnum.TWELVE_HOUR.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofHours(12)));
+		cacheConfigurationMap.put(CacheNameEnum.ONE_DAY.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(1)));
+		cacheConfigurationMap.put(CacheNameEnum.SEVEN_DAY.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(7)));
+		cacheConfigurationMap.put(CacheNameEnum.FOURTEEN_DAY.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(14)));
+		cacheConfigurationMap.put(CacheNameEnum.ONE_MONTH.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(31)));
+		cacheConfigurationMap.put(CacheNameEnum.THREE_MONTH.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(93)));
+		cacheConfigurationMap.put(CacheNameEnum.SIX_MONTH.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(182)));
+		cacheConfigurationMap.put(CacheNameEnum.ONE_YEAR.name(), commonRedisCacheConfiguration.entryTtl(Duration.ofDays(366)));
+		cacheConfigurationMap.put(CacheNameEnum.FOREVER.name(), commonRedisCacheConfiguration.entryTtl(Duration.ZERO));
 		// RedisCacheManager
 		RedisCacheManager redisCacheManager = RedisCacheManager.builder(clusterLettuceConnectionFactory)
-				.cacheDefaults(oneDayRedisCacheConfiguration)
 				.withInitialCacheConfigurations(cacheConfigurationMap)
 				.transactionAware()
 				// 不允许添加除上述定义之外的缓存名称
