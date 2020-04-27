a013be10b8c62e8e8a699d452661201ff3b6b169
hushirui <hushirui-hb@bestpay.com.cn>
2020-04-27 11:20:28 +0800

    .

--- spring.boot.starter.api/src/main/java/com/mrathena/spring/boot/starter/api/business/y2020/customer/dto/CustomerDTO.java
+++ spring.boot.starter.api/src/main/java/com/mrathena/spring/boot/starter/api/business/y2020/customer/dto/CustomerDTO.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 
 @Getter
 @Setter
-@ToString
+@ToString(callSuper = true)
 public class CustomerDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
