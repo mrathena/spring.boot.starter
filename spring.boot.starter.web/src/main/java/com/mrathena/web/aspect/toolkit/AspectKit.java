cc85680915bd4f16fdab45467a223e06493ec727
mrathena <git@mrathena.com>
2020-08-12 13:10:39 +0800

    .

--- spring.boot.starter.web/src/main/java/com/mrathena/web/aspect/toolkit/AspectKit.java
+++ spring.boot.starter.web/src/main/java/com/mrathena/web/aspect/toolkit/AspectKit.java
@@ -1,5 +1,7 @@
 package com.mrathena.web.aspect.toolkit;
 
+import com.mrathena.common.constant.Constant;
+import com.mrathena.common.entity.Response;
 import com.mrathena.common.toolkit.IdKit;
 import com.mrathena.common.toolkit.LogKit;
 import com.mrathena.spring.boot.starter.api.business.BaseReqDTO;
@@ -11,11 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author mrathena on 2020-03-29 02:26
@@ -24,10 +22,6 @@ import java.util.stream.Collectors;
 public final class AspectKit {
 
 	private AspectKit() {}
-
-	private static final List<Class<?>> CLASS_LIST = Arrays.asList(Byte.class, Short.class, Integer.class, Long.class, Float.class, Double.class, Boolean.class, Character.class,
-			byte.class, short.class, int.class, long.class, float.class, double.class, boolean.class, char.class,
-			String.class, Date.class, LocalDateTime.class, LocalDate.class, LocalTime.class);
 
 	public static Logger getLogger(ProceedingJoinPoint point) {
 		Class<?> clazz = point.getTarget().getClass();
@@ -69,79 +63,57 @@ public final class AspectKit {
 		}
 	}
 
-	public static String getRequestStr(ProceedingJoinPoint point) {
+	public static Object getRequest(ProceedingJoinPoint point) {
 		try {
 			Object[] args = point.getArgs();
 			if (ArrayUtils.isEmpty(args)) {
 				return null;
 			}
-			return Arrays.stream(args).map(AspectKit::getAllFieldNameAndValueStr).collect(Collectors.joining(";"));
+			List<String> argList = new LinkedList<>();
+			for (Object arg : args) {
+				if (null != arg) {
+					argList.add(arg.toString().replace(Constant.BLANK, Constant.EMPTY));
+				}
+			}
+			return argList.size() == 1 ? argList.get(0) : argList;
 		} catch (Throwable cause) {
-			return "UNKNOWN";
+			return Constant.ERROR;
 		}
 	}
 
-	private static String getAllFieldNameAndValueStr(Object object) {
-		if (null == object) {
-			return "null";
-		}
-		Class<?> clazz = object.getClass();
-		if (CLASS_LIST.contains(clazz)) {
-			return String.valueOf(object);
-		}
-		String simpleTypeName = clazz.getTypeName().substring(clazz.getTypeName().lastIndexOf(".")).replace(".", "");
-		Map<String, Object> fieldMap = new HashMap<>();
+	public static Object getResponse(Object object) {
 		try {
-			//获取本身和父级对象
-			for (; clazz != Object.class; clazz = clazz.getSuperclass()) {
-				//获取所有字段
+			if (null == object) {
+				return null;
+			}
+			if ((object instanceof Collection) || object instanceof Map) {
+				return "CollectionOrMap";
+			}
+			if (object instanceof Response) {
+				Class<?> clazz = object.getClass();
 				Field[] fields = clazz.getDeclaredFields();
+				Map<String, Object> fieldMap = new HashMap<>();
 				for (Field field : fields) {
 					field.setAccessible(true);
 					String fieldName = field.getName();
+					Object fieldValue = field.get(object);
 					if ("serialVersionUID".equals(fieldName)) {
 						continue;
 					}
-					if (null == fieldMap.get(field.getName())) {
-						fieldMap.put(fieldName, field.get(object));
+					if ("result".equals(fieldName)) {
+						if ((fieldValue instanceof Collection) || fieldValue instanceof Map) {
+							fieldValue = "CollectionOrMap";
+						}
 					}
+					fieldMap.put(fieldName, fieldValue);
 				}
+				return fieldMap.toString().replace(Constant.BLANK, Constant.EMPTY);
 			}
-			String collect = fieldMap.entrySet().stream().filter(entry -> entry.getValue() != null).map(entry -> entry.getKey() + "=" + entry.getValue()).collect(Collectors.joining(","));
-			return simpleTypeName + "(" + collect + ")";
+			return object;
 		} catch (Throwable cause) {
-			return null;
-		}
-	}
-
-	public static String getResponseStr(Object object) {
-		try {
-			Class<?> clazz = object.getClass();
-			if (CLASS_LIST.contains(clazz)) {
-				return String.valueOf(object);
-			}
-			String simpleClassName = clazz.getSimpleName();
-			Field[] fields = clazz.getDeclaredFields();
-			Map<String, Object> fieldMap = new HashMap<>();
-			for (Field field : fields) {
-				field.setAccessible(true);
-				String fieldName = field.getName();
-				Object fieldValue = field.get(object);
-				if ("serialVersionUID".equals(fieldName) || null == fieldValue) {
-					continue;
-				}
-				if ("result".equals(fieldName)) {
-					if ((fieldValue instanceof Collection) || fieldValue instanceof Map) {
-						fieldValue = "Collection or Map";
-					}
-				}
-				fieldMap.put(fieldName, fieldValue);
-			}
-			String collect = fieldMap.entrySet().stream().map(entry -> entry.getKey() + "=" + entry.getValue()).collect(Collectors.joining(","));
-			return simpleClassName + "(" + collect + ")";
-		} catch (Throwable cause) {
-			return "UNKNOWN";
+			return Constant.ERROR;
 		}
 	}
 
 }
+
